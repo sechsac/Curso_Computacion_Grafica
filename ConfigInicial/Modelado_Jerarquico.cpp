0ab61bcd88e7c1d5ba4a81795185eb7e9990c004
Isabel Hernández <112595334+sechsac@users.noreply.github.com>
2026-09-13 22:20:30 -0600

    Finalización Previo 5

--- ConfigInicial/Modelado_Jerarquico.cpp
+++ ConfigInicial/Modelado_Jerarquico.cpp
@@ -29,7 +29,7 @@ rot = 0.0f;
 
 // PARA MANIPULAR EL MODELO (genera la articulación del brazo)
 //For model
-float	hombro = 0.0f, codo = 0.0f;
+float	hombro = 0.0f, codo = 0.0f, muneca = 0.0f, dedo1 = 0.0f, dedo2 = 0.0f;
 
 
 int main() {
@@ -237,6 +237,47 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 36);//B
 
 
+		// Model : Palma de la mano
+		model = glm::translate(modelTemp, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(muneca), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		// Pasando el pivote a donde se quiere dibujar la siguiente figura (la muñeca)
+		modelTemp2 = modelTemp = model = glm::translate(model, glm::vec3(0.25f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.5f, 1.0f, 1.0f));
+		color = glm::vec3(1.0f, 1.0f, 1.0f); // En RGB
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//C
+
+
+		// Model : Dedo 1 A
+		model = glm::translate(modelTemp, glm::vec3(0.25f, 0.35f, 0.375f));
+		model = glm::rotate(model, glm::radians(dedo1), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		// Pasando el pivote a donde se quiere dibujar la siguiente figura (la muñeca)
+		modelTemp = model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(0.0f, 1.0f, 1.0f); // En RGB
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//D
+
+
+		// Model : Dedo 1 B
+		model = glm::translate(modelTemp, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(dedo1), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		// Pasando el pivote a donde se quiere dibujar la siguiente figura (la muñeca)
+		model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(1.0f, 0.0f, 1.0f); // En RGB
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//E
+
+
+
+
 
 		glBindVertexArray(0);
 
@@ -280,6 +321,18 @@ int main() {
 		 codo += 0.018f;
 	 if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
 		 codo -= 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+		 muneca += 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+		 muneca -= 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+		 dedo1 += 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+		 dedo1 -= 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+		 dedo2 += 0.018f;
+	 if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+		 dedo2 -= 0.018f;
  }
 
 
